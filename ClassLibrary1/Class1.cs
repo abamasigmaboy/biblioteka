6e986807c468839f1a99d4785c79203d550836b6
Anxiety <Anxiety@209-11>
2025-12-08 13:37:38 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace MatrixLib
+{
+    public static class MatrixTasks
+    {
+        public static void SwapMinMaxOnMainDiagonal(double[,] a)
+        {
+            int n = a.GetLength(0);
+
+            int iMin = 0, iMax = 0;
+            double min = a[0, 0], max = a[0, 0];
+
+            for (int i = 1; i < n; i++)
+            {
+                double v = a[i, i];
+                if (v < min) { min = v; iMin = i; }
+                if (v > max) { max = v; iMax = i; }
+            }
+
+            double tmp = a[iMin, iMin];
+            a[iMin, iMin] = a[iMax, iMax];
+            a[iMax, iMax] = tmp;
+        }
+    }
+}
---
+++ библиотека/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MatrixLib;
+class Program
+{
+    static void Main()
+    {
+        int n = 7;
+        double[,] a = new double[n, n];
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                a[i, j] = rnd.NextDouble() *  10; 
+        Console.WriteLine("Исходная матрица:");
+        PrintMatrix(a);
+        MatrixTasks.SwapMinMaxOnMainDiagonal(a);
+        Console.WriteLine("\nПосле обмена min и max на главной диагонали:");
+        PrintMatrix(a);
+    }
+    static void PrintMatrix(double[,] a)
+    {
+        int n0 = a.GetLength(0);
+        int n1 = a.GetLength(1);
+        for (int i = 0; i < n0; i++)
+        {
+            for (int j = 0; j < n1; j++)
+                Console.Write($"{a[i, j]:F2}\t");
+            Console.WriteLine();
+        }
+    }
+}
